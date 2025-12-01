06e70e112f0e62342b483a91e0b0390b04fbe3ce
filodepa05 <fdepaulis.ieu2023@student.ie.edu>
2025-12-01 21:52:01 +0100

    stage and updated security

--- StageClass.py
+++ StageClass.py
@@ -3,37 +3,40 @@ import threading
 class Stage:
     def __init__(self, name):
         self.name = name
-        self.music = None  # artist will set this
-        self.neighbours = []  # needed for Search pathfinding
+        self.music = None  # Artist will set this
+        self.neighbours = []  # For Search pathfinding
+
+        # Track spectators at the stage
         self.states = {
             'all': {'list': [], 'lock': threading.Lock()},
             'dancing': {'list': [], 'lock': threading.Lock()}
         }
 
     def receive(self, spectator, states=[]):
-        # Move spectator into stage and apply states
+        # Move spectator into stage and assign proper states
         states = ['all'] + states
+
         for state in states:
             if state in self.states:
                 with self.states[state]['lock']:
                     self.states[state]['list'].append(spectator)
 
-        # Set spectator's current location   
+        # Update spectator current location
         spectator.location = self
 
     def remove(self, spectator):
-        # Useful if someone leaves the stage
+        # Remove spectator from all state lists
         for state in self.states:
             with self.states[state]['lock']:
                 if spectator in self.states[state]['list']:
                     self.states[state]['list'].remove(spectator)
 
-    def setMusic(self, music_type):
-        # Artist changes the music of the stage
-        self.music = music_type
+    def setMusic(self, genre):
+        # Artist sets the current music playing on this stage
+        self.music = genre
         print(f"Stage {self.name}: Music changed to {self.music}")
 
     def addNeighbour(self, other_location):
-        # Use for Search pathfinding
+        # Allow Search to navigate between locations
         if other_location not in self.neighbours:
             self.neighbours.append(other_location)
